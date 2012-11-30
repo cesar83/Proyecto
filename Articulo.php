67de3faa733219c49f40e2a020e50c1626d44e1c
LourdesT <louli.torres@gmail.com>
2012-11-30 20:17:12 -0200

    Update Articulo.php

---
+++ Articulo.php
@@ -0,0 +1,164 @@
+Formulario Articulo
+<?php
+/** sistemaTrabajo/table.Articulos__.php   ( table_form )
+ * 
+ * @author   ycube RAD Plus ( automatically Generated ) 
+ * 
+ */ 
+
+$Table->name  = "Articulos";
+$Table->index = "";
+
+$Table->Add(
+    array(
+        F_NAME_ => "id",
+        F_TYPE_ => "INT UNSIGNED",
+        F_NULL_ => "NOT NULL",
+        F_KEY_ => "PRI",
+        F_DEFAULT_ => "NULL",
+        F_EXTRA_ => "AUTO_INCREMENT"));
+
+$Table->Add(
+    array(
+        F_NAME_ => "Id_Articulo",
+        F_TYPE_ => "INT",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "Descripcion",
+        F_TYPE_ => "varchar(50)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "PrecioCompra",
+        F_TYPE_ => "INT",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "PrecioVenta",
+        F_TYPE_ => "INT",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "Stock",
+        F_TYPE_ => "INT",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "FechaAlta",
+        F_TYPE_ => "date",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstCodProd",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstDescripcion",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstPrecAdq",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstPrecVenta",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstCantExis",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstFechaProd",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "DetalleProvedor",
+        F_TYPE_ => "subform",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "1",
+        F_UNIQ_ => ""));
+
+?>
