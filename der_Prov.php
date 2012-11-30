6493e793aa80f0ca08585ba70160d0f2b85d0258
LourdesT <louli.torres@gmail.com>
2012-11-30 20:21:21 -0200

    Update der_Prov.php

--- der_Prov.php
+++ der_Prov.php
@@ -1,81 +1,75 @@
-crear formulario detalle proveedor
-<?php
-$Obj->name = "det_Prov";
-$Obj->alias = "Detalle Proveedor";
-$Obj->help = "Detalle Proveedor";
-$Obj->copy_from = "";
-$Obj->Inheritance = "";
-$Obj->File = "det_Prov";
-$Obj->Filter = "";
-$Obj->Sort = "";
-$Obj->p_insert = "";
-$Obj->p_change = "";
-$Obj->p_delete = "";
-$Obj->Zebra = "white,lightblue";
-$Obj->Noedit = "";
-$Obj->NoInsert = "";
-$Obj->Limit = "";
-$Obj->Add(
+c<?php
+/** sistemaTrabajo/table.det_Prov__.php   ( table_form )
+ * 
+ * @author     ycube RAD Plus ( automatically Generated ) 
+ * 
+ */ 
+
+$Table->name  = "det_Prov";
+$Table->index = "";
+
+$Table->Add(
+    array(
+        F_NAME_ => "id",
+        F_TYPE_ => "INT UNSIGNED",
+        F_NULL_ => "NOT NULL",
+        F_KEY_ => "PRI",
+        F_DEFAULT_ => "NULL",
+        F_EXTRA_ => "AUTO_INCREMENT"));
+
+$Table->Add(
     array(
         F_NAME_ => "id_Det_Prov",
-        F_ALIAS_ => "Id Detalle",
-        F_HELP_ => "Id Detalle",
-        F_TYPE_ => "text",
-        F_AUTONUM_ => "1",
-        F_BROW_ => "1",
-        F_ORD_ => "10",
-        G_SHOW_ => "1",
-        G_CHANGE_ => "1"));
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
 
-$Obj->Add(
+$Table->Add(
     array(
         F_NAME_ => "rucP",
-        F_ALIAS_ => "Ruc",
-        F_HELP_ => "Ruc",
-        F_TYPE_ => "text",
-        F_MAKE_QUERY_ => "1",
-        F_QUERY_ => "'SELECT ruc_p,nom_p,direc_p,telefono_p FROM proveedor WHERE ruc_p='+rucP.get()",
-        F_QUERY_REF_ => "rucP.hasChanged()",
-        F_BROW_ => "1",
-        F_REQUIRED_ => "1",
-        F_ORD_ => "20",
-        G_SHOW_ => "1",
-        G_CHANGE_ => "1"));
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
 
-$Obj->Add(
+$Table->Add(
     array(
         F_NAME_ => "nom_p",
-        F_ALIAS_ => "Proveedor",
-        F_HELP_ => "Proveedor",
-        F_TYPE_ => "formula",
-        F_BROW_ => "1",
-        F_ORD_ => "30",
-        F_FORMULA_ => "db('nom_p')",
-        G_SHOW_ => "1",
-        G_CHANGE_ => "1"));
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
 
-$Obj->Add(
+$Table->Add(
     array(
         F_NAME_ => "telefono_p",
-        F_ALIAS_ => "Telefono",
-        F_HELP_ => "Telefono",
-        F_TYPE_ => "formula",
-        F_BROW_ => "1",
-        F_ORD_ => "50",
-        F_FORMULA_ => "db('telefono_p')",
-        G_SHOW_ => "1",
-        G_CHANGE_ => "1"));
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
 
-$Obj->Add(
+$Table->Add(
     array(
         F_NAME_ => "direc_p",
-        F_ALIAS_ => "Direccion",
-        F_HELP_ => "Direccion",
-        F_TYPE_ => "formula",
-        F_BROW_ => "1",
-        F_ORD_ => "60",
-        F_FORMULA_ => "db('direc_p')",
-        G_SHOW_ => "1",
-        G_CHANGE_ => "1"));
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
 
 ?>
