cb3a1cbe522cae5f500a5333075bd4a55d180213
cesar <benitezc36@gmail.com>
2012-11-30 20:31:09 -0200

    Update ventFact.php
    Modificacion

--- ventFact.php
+++ ventFact.php
@@ -1,1 +1,185 @@
-formulario veta facturada
+<?php
+/** sistemaTrabajo/table.DetVentFact__.php   ( table_form )
+ * 
+ * @author   ycube RAD Plus ( automatically Generated ) 
+ * 
+ */ 
+
+$Table->name  = "DetVentFact";
+$Table->index = "";
+
+$Table->Add(
+    array(
+        F_NAME_ => "id",
+        F_TYPE_ => "INT UNSIGNED",
+        F_NULL_ => "NOT NULL",
+        F_KEY_ => "PRI",
+        F_DEFAULT_ => "NULL",
+        F_EXTRA_ => "AUTO_INCREMENT"));
+
+$Table->Add(
+    array(
+        F_NAME_ => "IdFactDet",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "CodArt",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "Descripcion",
+        F_TYPE_ => "varchar(50)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "PrecioVenta",
+        F_TYPE_ => "varchar(10)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "Cantidad",
+        F_TYPE_ => "INT",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "SubTotal",
+        F_TYPE_ => "DOUBLE(5,2)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "DescActiva",
+        F_TYPE_ => "varchar(30)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstDescActiva",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstIdFactDet",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstIdCodArt",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstPrecVenta",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstCantidad",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstSubTotal",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "EstDescripcion",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "VisActiva",
+        F_TYPE_ => "subform",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "1",
+        F_UNIQ_ => ""));
+
+?>
