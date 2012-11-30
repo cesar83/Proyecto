4235078496b1f015cbe553db4255744dd46c6def
cesar <benitezc36@gmail.com>
2012-11-30 20:28:40 -0200

    Update proveedor.php
    Modificaciòn de la tabla

--- proveedor.php
+++ proveedor.php
@@ -1,1 +1,64 @@
-formulario Proveedor
+<?php
+/** sistemaTrabajo/table.proveedor__.php   ( table_form )
+ * 
+ * @author   ycube RAD Plus ( automatically Generated ) 
+ * 
+ */ 
+
+$Table->name  = "proveedor";
+$Table->index = "";
+
+$Table->Add(
+    array(
+        F_NAME_ => "id",
+        F_TYPE_ => "INT UNSIGNED",
+        F_NULL_ => "NOT NULL",
+        F_KEY_ => "PRI",
+        F_DEFAULT_ => "NULL",
+        F_EXTRA_ => "AUTO_INCREMENT"));
+
+$Table->Add(
+    array(
+        F_NAME_ => "ruc_p",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "nom_p",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "direc_p",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+$Table->Add(
+    array(
+        F_NAME_ => "telefono_p",
+        F_TYPE_ => "varchar(60)",
+        F_NULL_ => "",
+        F_KEY_ => "",
+        F_DEFAULT_ => "",
+        F_EXTRA_ => "",
+        F_NODB_ => "",
+        F_UNIQ_ => ""));
+
+?>
